b2f9715d1825bcfb35476b66c902fb6f3b79bdac
Lidor Repo <lidormalich@gmail.com>
2023-02-26 11:59:31 +0200

    update sms

--- index.js
+++ index.js
@@ -9,10 +9,7 @@ const port = 5000
 const routes = require('./routes/index')
 const bodyParser = require('body-parser')
 
-
-
-
-
+app.use(cors()) //Open To all
 require('dotenv').config();
 
 mongoose.Promise = global.Promise
@@ -20,29 +17,6 @@ mongoose.connect(process.env.DB, { useNewUrlParser: true })
     .then(() => { console.log('conected to Data base') })
     .catch((err) => console.log(err))
 app.use(bodyParser.json())
-
-// Cros
-app.use(cors()) //Open To all
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-
 app.use('/api', routes)
 
 app.use((err, req, res, next) => {
